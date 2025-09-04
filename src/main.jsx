5c27ed3cbb3ec52f5a05675fa35125205bdde8e2
ProjectBibo <rowan.gulsen@windesheim.nl>
2025-09-04 22:13:10 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css"; // pas toevoegen bij Tailwind
+import "./index.css"; // ✅ activeer dit voor Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
