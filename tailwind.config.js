81fe5afac90ab503ebd15d0a6a89cc21206159e1
ProjectBibo <rowan.gulsen@windesheim.nl>
2025-10-06 13:09:38 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          blue: "#004AAD", // hoofdkleur
+          yellow: "#F9C513",
+          red: "#E63946",
+          purple: "#3B2E7E",
+          graylight: "#F7F8FA",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
   plugins: [],
 };
