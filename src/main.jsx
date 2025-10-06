99129f7a7725f4a207adad44f63037ab42364ae1
ProjectBibo <rowan.gulsen@windesheim.nl>
2025-10-07 00:11:00 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import ErrorBoundary from "./ErrorBoundary";
+import App from "./App";
 import "./index.css";
-import ErrorBoundary from "./ErrorBoundary.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
