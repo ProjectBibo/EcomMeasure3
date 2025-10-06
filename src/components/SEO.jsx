90db660e62f946190e62b497ae6472c8f21bad9a
ProjectBibo <rowan.gulsen@windesheim.nl>
2025-10-06 13:31:05 +0200

    Create SEO.jsx

---
+++ src/components/SEO.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+/**
+ * SEO component
+ * Gebruik: <SEO title="Mijn titel" description="..." url="..." image="..." />
+ */
+export default function SEO({
+  title = "EcomMeasure – Data-gedreven website optimalisatie",
+  description = "EcomMeasure helpt webshops groeien met GA4, Consent Mode en UX optimalisaties.",
+  url = "https://www.ecommeasure.com",
+  image = "/og-image.png",
+}) {
+  return (
+    <Helmet>
+      {/* Title & Description */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content="EcomMeasure, GA4, Google Analytics, UX Research, CRO, website optimalisatie" />
+
+      {/* Open Graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Helmet>
+  );
+}
