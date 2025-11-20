56df860af42eab2abc1399849bd1db6e439cb0b5
ProjectBibo <rowan.gulsen@windesheim.nl>
2025-11-20 17:30:03 +0100

    Update Google tag ID

--- src/components/GoogleTag.jsx
+++ src/components/GoogleTag.jsx
@@ -5,14 +5,14 @@ export default function GoogleTag() {
   return (
     <Helmet>
       {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-8RLPEY8FCS" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3DQBW03013" />
       <script>
         {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-8RLPEY8FCS');
+  gtag('config', 'G-3DQBW03013');
 `}
       </script>
     </Helmet>
